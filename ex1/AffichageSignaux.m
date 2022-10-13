0708db23f33c0a3186c35a0e71eea6a79febb385
ESN <nicolas.esseiva@gmail.com>
2022-10-13 09:22:35 +0200

    Quelques changements AffichageSignaux

--- ex1/AffichageSignaux.m
+++ ex1/AffichageSignaux.m
@@ -11,24 +11,26 @@ x1 = 5*cos(2*pi*f*t);
 x2 = 4*exp(-t/tau).*cos(2*pi*f*t);
 
 %% Plot
+facteur_echelle = 1e3;
 
 axes(1) = subplot(2,1,1);
-plot(t, x1, 'LineWidth', 1.5, 'Color','b');
-ylabel('x_1(t)','FontSize',18);
-ylim([-5 5]);
+plot(facteur_echelle*t, x1, 'b', 'LineWidth', 1.5);
+set(axes(1), 'FontSize', 16); % Taille font des labels sur les axes
+ylabel('x_1(t)','FontSize',24);
+ylim([-6 6]);
 grid on;
 
 
-axes(2) = subplot(2,1,2);
-plot(t, x2, 'LineWidth', 1.5, 'Color','r');
-ylabel('x_2(t)','FontSize',18);
-xlabel('t [ms]','FontSize',18);
+axes(2) = subplot(212);
+plot(facteur_echelle*t, x2, 'r', 'LineWidth', 1.5);
+set(axes(2), 'FontSize', 16); % Taille font des labels sur les axes
+ylabel('x_2(t)','FontSize',24);
 ylim([-10 10]);
-yticks([-10 0 10]);
 grid
 grid on;
 
+xlabel('t [ms]','FontSize', 24);
 linkaxes(axes, 'x')
-xlim([-0.5e-3 1.5e-3]);
+xlim([-0.5 1.5]);
 
 
